64c852784d70ef7d6b595f0e3bbd1ab6ba307819
Student <student@Makerss-MacBook-Air.local>
2020-01-08 16:42:39 +0000

    refactored test code using helper

--- spec/features/game_setup_spec.rb
+++ spec/features/game_setup_spec.rb
@@ -1,19 +1,13 @@
 feature 'Enter names' do
   scenario 'Players navigate to homepage and enter their names' do
-    visit('/')
-    fill_in :player_1_name, with: "John"
-    fill_in :player_2_name, with: "Mark"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'John vs. Mark'
   end
 end
 
 feature 'displays hitpoints' do
   scenario 'hit points displayed after names are entered' do
-    visit('/')
-    fill_in :player_1_name, with: "John"
-    fill_in :player_2_name, with: "Mark"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mark: 60HP'
   end
 end
