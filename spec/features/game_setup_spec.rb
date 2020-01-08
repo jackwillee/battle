dde9a3b28679ec25abb873968bc8a5684d26b56f
Student <student@Makerss-MacBook-Air.local>
2020-01-08 16:15:44 +0000

    displays points

--- spec/features/game_setup_spec.rb
+++ spec/features/game_setup_spec.rb
@@ -7,3 +7,13 @@ feature 'Enter names' do
     expect(page).to have_content 'John vs. Mark'
   end
 end
+
+feature 'displays hitpoints' do
+  scenario 'hit points displayed after names are entered' do
+    visit('/')
+    fill_in :player_1_name, with: "John"
+    fill_in :player_2_name, with: "Mark"
+    click_button 'Submit'
+    expect(page).to have_content 'Mark: 60HP'
+  end
+end
