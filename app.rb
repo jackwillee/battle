e77e7087ed9d9487ee52fe1ab0ce36fcf03a39d4
ratcliffetj <ratcliffetj89@gmail.com>
2020-01-08 17:40:41 +0000

    Added attack functionality with HP data stored in session

--- app.rb
+++ app.rb
@@ -9,15 +9,37 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     redirect '/play'
+  end
+
+  get '/attack' do
+    session[:turn] = session[:turn] + 1
+    (session[:turn])%2 == 1 ? session[:player_1_HP] = session[:player_1_HP] - 10 : session[:player_2_HP] = session[:player_2_HP] - 10
+    redirect '/play_continued'
   end
 
   get '/play' do 
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
+    session[:turn] = 1
+    session[:player_1_HP] = 60
+    session[:player_2_HP] = 60
+    @player_1_HP = session[:player_1_HP]
+    @player_2_HP = session[:player_2_HP]
     erb(:play)
-  
+  end
 
+  get '/play_continued' do 
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    @player_1_HP = session[:player_1_HP]
+    @player_2_HP = session[:player_2_HP]
+    if session[:turn]>1
+      (session[:turn])%2 == 1 ? @message = "You attacked player 1" : @message = "You attacked player 2"
+    end
+    erb(:play)
   end 
 
   # start the server if ruby file executed directly
